59e9891e020b166548df41860b7f30303c31d96e
ancamil-sys <fr.ancamil@duocuc.cl>
2026-03-18 21:35:23 -0300

    Mi primer commit

---
+++ src/test/java/holamundo1/holamundo1/Holamundo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package holamundo1.holamundo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Holamundo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
